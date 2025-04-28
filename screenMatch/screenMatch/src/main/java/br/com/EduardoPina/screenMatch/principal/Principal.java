78b8a3813ffd8ba0de2a251bd82b20066f62bd43
Eduardo Fleury <eduardopinafleury@gmail.com>
2025-04-28 15:34:41 -0300

    Evoluindo aplicação

--- screenMatch/screenMatch/src/main/java/br/com/EduardoPina/screenMatch/principal/Principal.java
+++ screenMatch/screenMatch/src/main/java/br/com/EduardoPina/screenMatch/principal/Principal.java
@@ -21,6 +21,8 @@ public class Principal {
     private SerieRepository repository;
     List<Serie> series = new ArrayList<>();
 
+    private Optional<Serie> serieBusca;
+
     public Principal(SerieRepository repository) {
         this.repository = repository;
     }
@@ -32,6 +34,15 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar séries buscadas
+                    4 - Buscar série por titulo
+                    5 - Buscar série por Ator
+                    6 - Buscar Top 5 séries
+                    7 - Buscar séries por categoria
+                    8 - Buscar séries por temporadas / Avaliacao
+                    9 - Buscar episodios por trecho do nome
+                    10- Top Episodios por serie
+                    11- Episodios a partir de uma data
+                    
 
                     0 - Sair                                 
                     """;
@@ -49,6 +60,30 @@ public class Principal {
                     break;
                 case 3:
                     ListarSerieBuscada();
+                    break;
+                case 4:
+                    buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriePorAutor();
+                    break;
+                case 6:
+                    buscarTop5Series();
+                    break;
+                case 7:
+                    buscarSeriesPorCategoria();
+                    break;
+                case 8:
+                    buscarSeriesPorTemporadaAvaliacao();
+                    break;
+                case 9:
+                    buscarEpisodioPorTrecho();
+                    break;
+                case 10:
+                    topEpisodiosPorSerie();
+                    break;
+                case 11:
+                    buscarEpisodiosDepoisDeUmaData();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -81,8 +116,7 @@ public class Principal {
         System.out.println("Escolha uma serie pelo nome: ");
        var nomeSerie = leitura.nextLine();
 
-       Optional<Serie> serie = series.stream().filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-               .findFirst();
+       Optional<Serie> serie = repository.findByTituloContainingIgnoreCase(nomeSerie);
 
        if(serie.isPresent()){
             var serieEncontrada = serie.get();
@@ -109,6 +143,109 @@ public class Principal {
           series = repository.findAll();
 //        dadosSeries.stream().map(d -> new Serie(d)).collect(Collectors.toList());
         series.stream().sorted(Comparator.comparing(Serie::getGenero)).forEach(System.out::println);
+    }
+
+    private void buscarSeriePorTitulo(){
+        System.out.println("Escolha uma serie pelo nome: ");
+        var nomeSerie = leitura.nextLine();
+
+        serieBusca = repository.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if(serieBusca.isPresent()){
+            System.out.println("Dados da série: "+serieBusca.get());
+        }else{
+            System.out.println("Série não encontrada.");
+        }
+    }
+
+    private void buscarSeriePorAutor(){
+        System.out.println("Escolha uma serie pelo nome do ator: ");
+        var nomeAtor = leitura.nextLine();
+
+//        List<Serie> serieBuscada = repository.findByAtoresContainingIgnoreCase(nomeAtor);
+//
+//        System.out.println("Dados das Séries em que "+nomeAtor+" trabalhou.");
+//        serieBuscada.forEach(s -> System.out.println(s.getTitulo() + " Avaliação: "+s.getAvaliacao()));
+        System.out.println("Avaliação a partir de que valor? ");
+        var avaliacao = leitura.nextDouble();
+
+        List<Serie> melhoresSeries = repository.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+
+        System.out.println("Dados das Séries em que "+nomeAtor+" trabalhou.");
+        melhoresSeries.forEach(s -> System.out.println(s.getTitulo() + " Avaliação: "+s.getAvaliacao()));
+
+    }
+
+    private void buscarTop5Series(){
+        List<Serie> melhoresSeries = repository.findTop5ByOrderByAvaliacaoDesc();
+
+        System.out.println("Dados das Top 5 melhores Séries.");
+        melhoresSeries.forEach(s -> System.out.println(s.getTitulo() + " Avaliação: "+s.getAvaliacao()));
+    }
+
+    private void buscarSeriesPorCategoria(){
+        System.out.println("Escolha uma categoria: ");
+        var nomeGenero = leitura.nextLine();
+
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        List<Serie> seriesPorCategoria = repository.findByGenero(categoria);
+
+        System.out.println("Dados das Séries por Categoria ("+categoria+")");
+        seriesPorCategoria.forEach(s -> System.out.println(s.getTitulo() + " Avaliação: "+s.getAvaliacao()));
+    }
+
+    private void buscarSeriesPorTemporadaAvaliacao(){
+        System.out.println("Qual numero de temporadas maximo que deseja? ");
+        var temporadas = leitura.nextInt();
+
+        System.out.println("Avaliação maior que? ");
+        var avaliacao = leitura.nextDouble();
+
+//        List<Serie> seriesBuscadas = repository.findBytotaltemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(temporadas, avaliacao);
+        List<Serie> seriesBuscadas = repository.seriePorTemporadaEAvaliacao(temporadas, avaliacao);
+
+
+        System.out.println("Dados das Séries Encontradas: ");
+        seriesBuscadas.forEach(s -> System.out.println(s.getTitulo() + " Avaliação: "+s.getAvaliacao()));
+
+    }
+
+    private void buscarEpisodioPorTrecho(){
+        System.out.println("Digite o trecho do nome do episodio:  ");
+        var trechosEpisodios = leitura.nextLine();
+
+        List<Episodio> episodiosEncontrados = repository.episodiosPorTrecho(trechosEpisodios);
+
+        System.out.println("Dados dos episodios: ");
+        episodiosEncontrados.forEach(s -> System.out.println("Serie "+ s.getSerie().getTitulo() +" "+s.getTitulo() + " Avaliação: "+s.getAvaliacao()));
+    }
+
+    private void topEpisodiosPorSerie(){
+       buscarSeriePorTitulo();
+       if(serieBusca.isPresent()){
+           Serie serie = serieBusca.get();
+           List<Episodio> topEpisodios = repository.topEpisodiosPorSerie(serie);
+
+           System.out.println("Dados dos episodios: ");
+           topEpisodios.forEach(e -> System.out.println("Serie "+ e.getSerie().getTitulo() +" "+e.getTitulo() + " Avaliação: "+e.getAvaliacao()));
+
+       }
+    }
+
+    private void buscarEpisodiosDepoisDeUmaData(){
+        buscarSeriePorTitulo();
+        if(serieBusca.isPresent()){
+            System.out.println("Digite o ano limite de lançamento: ");
+            var anoLancamento = leitura.nextInt();
+            leitura.nextLine();
+
+            Serie serie = serieBusca.get();
+            List<Episodio> episodiosAno = repository.episodiosPorAnoESerie(serie, anoLancamento);
+
+            System.out.println("Dados dos episodios: ");
+            episodiosAno.forEach(e -> System.out.println("Serie "+ e.getSerie().getTitulo() +" "+e.getTitulo() + " Avaliação: "+e.getAvaliacao()));
+
+        }
     }
 
 }
--- screenMatch/screenMatch/src/main/java/br/com/EduardoPina/screenMatch/repository/SerieRepository.java
+++ screenMatch/screenMatch/src/main/java/br/com/EduardoPina/screenMatch/repository/SerieRepository.java
@@ -1,10 +1,37 @@
 package br.com.EduardoPina.screenMatch.repository;
 
+import br.com.EduardoPina.screenMatch.model.Categoria;
+import br.com.EduardoPina.screenMatch.model.Episodio;
 import br.com.EduardoPina.screenMatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Long>{
 
+   Optional<Serie> findByTituloContainingIgnoreCase(String nomeserie);
+   List<Serie> findByAtoresContainingIgnoreCase(String nomeAtor);
+
+   List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
 
 
+   List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+   List<Serie> findByGenero(Categoria categoria);
+
+   List<Serie> findBytotaltemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(Integer temporadas, Double avaliacao);
+
+   @Query("select s from Serie s WHERE s.totaltemporadas <= :Totaltemporadas AND s.avaliacao >= :avaliacao")
+   List<Serie> seriePorTemporadaEAvaliacao(Integer Totaltemporadas, Double avaliacao);
+
+   @Query("select e from Serie s Join s.episodios e WHERE e.titulo ILIKE %:trechosEpisodios% ")
+   List<Episodio> episodiosPorTrecho(String trechosEpisodios);
+
+   @Query("select e from Serie s Join s.episodios e WHERE s = :serie ORDER BY e.avaliacao DESC LIMIT 5 ")
+   List<Episodio> topEpisodiosPorSerie(Serie serie);
+
+   @Query("select e from Serie s Join s.episodios e WHERE s = :serie AND YEAR(e.datalancamento) >= :anoLancamento ")
+   List<Episodio> episodiosPorAnoESerie(Serie serie, int anoLancamento);
 }
